978e420d44fae1a2c8bdc4a8ee2b63d35bc1e041
Andres Villca <andres24vt@gmail.com>
2023-02-07 19:58:00 +0100

    header

--- header.php
+++ header.php
@@ -1,18 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <title><?php echo wp_get_document_title(); ?></title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" type="text/css" media="screen" />
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
     <?php wp_head() ?>
 </head>
 <body <?php body_class()?>>
 <?php wp_body_open()?>
-    <header>
-        Header
+    
+    <header id="header" role="banner">
+        <h2><a href="<?php echo home_url(); ?>/"><?php bloginfo( 'name' ); ?></a></h2>
+        <div class="description"><?php bloginfo( 'description' ); ?></div>
     </header>
+
     <nav>
         Nav
     </nav>
