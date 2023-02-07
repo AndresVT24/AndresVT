e7cea099d40c5690ed7067b4c98d8b85f22fc5c8
Andres Villca <andres24vt@gmail.com>
2023-02-07 19:46:44 +0100

    title

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <title><?php echo wp_get_document_title(); ?></title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" type="text/css" media="screen" />
