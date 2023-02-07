725aa46f39dc36edc7172d2742f8102edfa2f44d
Andres Villca <andres24vt@gmail.com>
2023-02-07 20:15:39 +0100

    functions.php

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+if ( ! function_exists( 'andresvt_register_nav_menu' ) ) {
+
+    function andresvt_register_nav_menu(){
+        register_nav_menus( array(
+            'primary_menu' => __( 'Primary Menu', 'andresvt' ),
+            'footer_menu'  => __( 'Footer Menu', 'andresvt' ),
+        ) );
+    }
+    add_action( 'after_setup_theme', 'andresvt_register_nav_menu', 0 );
+}
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
             the_post();
         ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <h1><?php the_title(sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>');?></h1>
+        <?php the_title(sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>');?>
         <div class="entry-content">
         <?php the_content();
             the_excerpt() ;
