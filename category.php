48b6b5096c3ac23a5273d3fcf122cab3088b67a3
Andres Villca <andres24vt@gmail.com>
2023-02-14 20:09:29 +0100

    style and img

--- category.php
+++ category.php
@@ -5,6 +5,11 @@
             the_post();
         ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <?php
+            if ( has_post_thumbnail() ) {
+                the_post_thumbnail();
+            }
+        ?>
         <?php the_title(sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>');?>
         <div class="entry-content">
         <?php the_content();
