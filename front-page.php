434d8072e8d7d2709c267503e39a0c8323fb80d2
Andres Villca <andres24vt@gmail.com>
2023-02-14 19:18:51 +0100

    a

--- front-page.php
+++ front-page.php
@@ -16,6 +16,27 @@
             <?php
                 endwhile;
             ?>
+            <?php
+                echo "<section><h3>Últimas entradas de XXXX</h3>";
+            $args = array(
+            'numberposts' => 3,
+            'category' => 2,
+            );
+            $myposts = get_posts( $args );
+            foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+            <li>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><h4>
+            <div class="entry-content">
+            <p><strong><small><?php the_date(); ?></small></strong></p>
+            <p><?php the_excerpt(); ?></p>
+            </div><!-- entry-content -->
+            </article>
+            </li>
+            <?php endforeach;
+            wp_reset_postdata();
+            echo "</section>";
+            ?>
         </main>
         <?php get_sidebar()?>
     </div>
