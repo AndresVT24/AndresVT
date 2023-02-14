2a25c4df8a87d8fe9231dd6bb2e2e3240fb7b93e
Andres Villca <andres24vt@gmail.com>
2023-02-14 20:22:38 +0100

    404

--- 404.php
+++ 404.php
@@ -1,4 +1,7 @@
 <?php get_header();?>
-    <h1>Ooops, The page you requested was not found</h1>
+<main>
+    <h1>ERROR 404</h1>
+    <h2>Ooops, The page you requested was not found</h2>
     <?php get_search_form()?>
+<main>
 <?php get_footer();?>
