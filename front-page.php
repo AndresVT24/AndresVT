88eda7456c8d36d54787d5fc6118b9fd21073772
Andres Villca <andres24vt@gmail.com>
2023-02-14 19:27:27 +0100

    a

--- front-page.php
+++ front-page.php
@@ -20,25 +20,25 @@
             </main>
             <section>
                 <h3>Últimas entradas de XXXX</h3>
-            <?php
-            $args = array(
-            'numberposts' => 3,
-            'category' => 2,
-            );
-            $myposts = get_posts( $args );
-            foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
-            <li>
-            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><h4>
-            <div class="entry-content">
-            <p><strong><small><?php the_date(); ?></small></strong></p>
-            <p><?php the_excerpt(); ?></p>
-            </div><!-- entry-content -->
-            </article>
-            </li>
-            <?php endforeach;
-            wp_reset_postdata();
-            ?>
+                <?php
+                $args = array(
+                'numberposts' => 3,
+                'category' => 2,
+                );
+                $myposts = get_posts( $args );
+                foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+                <li>
+                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                        <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+                        <div class="entry-content">
+                            <p><strong><small><?php the_date(); ?></small></strong></p>
+                            <p><?php the_excerpt(); ?></p>
+                        </div><!-- entry-content -->
+                    </article>
+                </li>
+                <?php endforeach;
+                wp_reset_postdata();
+                ?>
             </section>
         </div>
         
