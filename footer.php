4c9b5d4723dae2df731ff1b98ae96f93c9ad2179
Andres Villca <andres24vt@gmail.com>
2023-02-14 18:53:18 +0100

    changes

--- footer.php
+++ footer.php
@@ -1,5 +1,9 @@
-<footer>
-        Footer
+    <footer>
+    <?php
+        wp_nav_menu( array(
+            'theme_location' => 'secondary_menu'
+        ) );
+    ?>
     </footer>
     <?php wp_footer()?>
 </body>
--- front-page.php
+++ front-page.php
@@ -1,22 +1,23 @@
-    <?php get_header();?>
-    <main>front-page.php
-        <?php
-            while ( have_posts() ) :
-            the_post();
-        ?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <?php the_title(sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>');?>
-        <div class="entry-content">
-        <?php the_content();
-            the_excerpt() ;
-        ?>
-        </div><!-- entry-content -->
-        </article>
-        <?php
-            endwhile;
-        ?>
-    </main>
-    <?php get_sidebar()?>
-
+    <div>
+        <?php get_header();?>
+        <main>front-page.php
+            <?php
+                while ( have_posts() ) :
+                the_post();
+            ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <?php the_title(sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>');?>
+            <div class="entry-content">
+            <?php the_content();
+                the_excerpt() ;
+            ?>
+            </div><!-- entry-content -->
+            </article>
+            <?php
+                endwhile;
+            ?>
+        </main>
+        <?php get_sidebar()?>
+    </div>
 
     <?php get_footer();?>
