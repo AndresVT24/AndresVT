6bc35e4a8790fc3e56479f42041a747beed97362
Andres Villca <andres24vt@gmail.com>
2023-02-14 19:40:12 +0100

    style

---
+++ page.php
@@ -0,0 +1,19 @@
+<?php get_header();?>
+    <main>index.php
+        <?php
+            while ( have_posts() ) :
+            the_post();
+        ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <?php the_title(sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>');?>
+        <div class="entry-content">
+        <?php the_content();
+            the_excerpt() ;
+        ?>
+        </div><!-- entry-content -->
+        </article>
+        <?php
+            endwhile;
+        ?>
+    </main>
+    <?php get_footer();?>
