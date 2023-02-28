624216a1089dea7f3230b910ed081d5c7904f153
Andres Villca <andres24vt@gmail.com>
2023-02-28 20:37:26 +0100

    css

--- category.php
+++ category.php
@@ -1,23 +1,28 @@
 <?php get_header();?>
-    <main>category.php
-        <?php
-            while ( have_posts() ) :
-            the_post();
-        ?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <?php
-            if ( has_post_thumbnail() ) {
-                the_post_thumbnail();
-            }
-        ?>
-        <?php the_title(sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>');?>
-        <div class="entry-content">
-        <?php the_content();
-        ?>
-        </div><!-- entry-content -->
-        </article><hr>
-        <?php
-            endwhile;
-        ?>
-    </main>
+<div class="maquetarSidebar">
+    <div class="maquetarMainSection">
+        <main>category.php
+            <?php
+                while ( have_posts() ) :
+                the_post();
+            ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <?php
+                if ( has_post_thumbnail() ) {
+                    the_post_thumbnail();
+                }
+            ?>
+            <?php the_title(sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>');?>
+            <div class="entry-content">
+            <?php the_content();
+            ?>
+            </div><!-- entry-content -->
+            </article><hr>
+            <?php
+                endwhile;
+            ?>
+        </main>
+    </div>
+    <?php get_sidebar()?>
+</div>
     <?php get_footer();?>
